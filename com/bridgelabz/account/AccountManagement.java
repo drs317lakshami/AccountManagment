7f21631c44b8d0b5c728021540b9c71d473eae55
drs317lakshami <devrajkumar317@gmail.com>
2022-12-15 08:58:02 +0530

    Welcome to Account Mangement System

---
+++ com/bridgelabz/account/AccountManagement.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.account;
+
+import java.util.Scanner;
+
+public class AccountManagement {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Welcome to account management system");
+		
+	}
+}
